0305d89bca006b8f1660226ee9c301f7f48789cd
Sumito Izumita <sumito@izumita.com>
2020-07-01 19:24:41 +0900

    VoiceControllerの入室機能作成・ライブラリの参照先変更

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Bard.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Bard, []}
     ]
   end
 
@@ -23,9 +24,9 @@ defmodule Bard.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:alchemy, git: "git@github.com:sizumita/alchemy.git"},
-      {:google_api_storage, "~> 0.19.0"},
-      {:goth, "~> 1.2.0"}
+      {:alchemy, git: "https://github.com/dongmaster/alchemy.git"}
+      # {:google_api_storage, "~> 0.19.0"},
+      # {:goth, "~> 1.2.0"}
     ]
   end
 end
